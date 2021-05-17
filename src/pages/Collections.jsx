5fa9cd07bdd314fb9533255a621fe18ff71193da
deannovri <novridean@gmail.com>
2021-05-17 21:37:15 +0700

    update terbaru

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,12 +1,248 @@
 import React from 'react'
 import Header from '../components/Header'
+import { CgOptions } from 'react-icons/cg'
+import Dummy3 from '../assets/img/dummy-img/dummy3.png'
+import { BsCardImage } from 'react-icons/bs'
+import { BsBoxArrowInDown } from 'react-icons/bs'
+import { AiOutlineEye } from 'react-icons/ai'
+import { AiFillEdit } from 'react-icons/ai'
 
 function Collections() {
 	return (
 		<>
 		<Header />
-		<div>
-			
+		<div className="collections-container">
+			<div className="collections-inner-container">
+				<div className="collection-first-inner-container">
+						<div className="collections-title">Collections</div>
+						<button className="collections-button">New Collection</button>
+				</div>
+				<div className="collection-second-inner-container">
+					<input className="collections-searchbar" type="search" placeholder="Search collections"/>
+					<div className="collections-filter">
+						<div><CgOptions/></div>
+						<div>Filter</div>
+					</div>
+				</div>
+				<div className="collections-third-inner-container">
+					<div className="collections-cards">
+						<div><img className="collections-cards-img" src={Dummy3} alt="" /></div>
+						<div className="collections-cards-text-1">
+							<div>
+								<div>Justin & Stella</div>
+								<div className="collections-cards-text-1-date">28 March 2021</div>
+							</div>
+							<div>
+								<div className="collections-cards-text-1-preview">
+									<AiOutlineEye className="cct1p-img" />
+									<div className="cct1p-text">Preview</div>
+								</div>
+							</div>
+						</div>
+						<div className="collections-cards-text-2">
+							<div className="collections-cards-text-2-imgdow">
+								<BsCardImage className="cct2p-img"/>
+								<div>13</div>
+								<BsBoxArrowInDown className="cct2p-img2"/>
+								<div>3</div>
+							</div>
+							<div className="collections-cards-text-2-edit">		
+								<AiFillEdit className="cct2p-img3"/>
+								<div className="cct2p-text">Edit</div>
+							</div>
+						</div>
+					</div>
+					<div className="collections-cards">
+						<div><img className="collections-cards-img" src={Dummy3} alt="" /></div>
+						<div className="collections-cards-text-1">
+							<div>
+								<div>Justin & Stella</div>
+								<div className="collections-cards-text-1-date">28 March 2021</div>
+							</div>
+							<div>
+								<div className="collections-cards-text-1-preview">
+									<AiOutlineEye className="cct1p-img" />
+									<div className="cct1p-text">Preview</div>
+								</div>
+							</div>
+						</div>
+						<div className="collections-cards-text-2">
+							<div className="collections-cards-text-2-imgdow">
+								<BsCardImage className="cct2p-img"/>
+								<div>13</div>
+								<BsBoxArrowInDown className="cct2p-img2"/>
+								<div>3</div>
+							</div>
+							<div className="collections-cards-text-2-edit">		
+								<AiFillEdit className="cct2p-img3"/>
+								<div className="cct2p-text">Edit</div>
+							</div>
+						</div>
+					</div>
+					<div className="collections-cards">
+						<div><img className="collections-cards-img" src={Dummy3} alt="" /></div>
+						<div className="collections-cards-text-1">
+							<div>
+								<div>Justin & Stella</div>
+								<div className="collections-cards-text-1-date">28 March 2021</div>
+							</div>
+							<div>
+								<div className="collections-cards-text-1-preview">
+									<AiOutlineEye className="cct1p-img" />
+									<div className="cct1p-text">Preview</div>
+								</div>
+							</div>
+						</div>
+						<div className="collections-cards-text-2">
+							<div className="collections-cards-text-2-imgdow">
+								<BsCardImage className="cct2p-img"/>
+								<div>13</div>
+								<BsBoxArrowInDown className="cct2p-img2"/>
+								<div>3</div>
+							</div>
+							<div className="collections-cards-text-2-edit">		
+								<AiFillEdit className="cct2p-img3"/>
+								<div className="cct2p-text">Edit</div>
+							</div>
+						</div>
+					</div>
+					<div className="collections-cards">
+						<div><img className="collections-cards-img" src={Dummy3} alt="" /></div>
+						<div className="collections-cards-text-1">
+							<div>
+								<div>Justin & Stella</div>
+								<div className="collections-cards-text-1-date">28 March 2021</div>
+							</div>
+							<div>
+								<div className="collections-cards-text-1-preview">
+									<AiOutlineEye className="cct1p-img" />
+									<div className="cct1p-text">Preview</div>
+								</div>
+							</div>
+						</div>
+						<div className="collections-cards-text-2">
+							<div className="collections-cards-text-2-imgdow">
+								<BsCardImage className="cct2p-img"/>
+								<div>13</div>
+								<BsBoxArrowInDown className="cct2p-img2"/>
+								<div>3</div>
+							</div>
+							<div className="collections-cards-text-2-edit">		
+								<AiFillEdit className="cct2p-img3"/>
+								<div className="cct2p-text">Edit</div>
+							</div>
+						</div>
+					</div>
+						<div className="collections-cards">
+						<div><img className="collections-cards-img" src={Dummy3} alt="" /></div>
+						<div className="collections-cards-text-1">
+							<div>
+								<div>Justin & Stella</div>
+								<div className="collections-cards-text-1-date">28 March 2021</div>
+							</div>
+							<div>
+								<div className="collections-cards-text-1-preview">
+									<AiOutlineEye className="cct1p-img" />
+									<div className="cct1p-text">Preview</div>
+								</div>
+							</div>
+						</div>
+						<div className="collections-cards-text-2">
+							<div className="collections-cards-text-2-imgdow">
+								<BsCardImage className="cct2p-img"/>
+								<div>13</div>
+								<BsBoxArrowInDown className="cct2p-img2"/>
+								<div>3</div>
+							</div>
+							<div className="collections-cards-text-2-edit">		
+								<AiFillEdit className="cct2p-img3"/>
+								<div className="cct2p-text">Edit</div>
+							</div>
+						</div>
+					</div>
+						<div className="collections-cards">
+						<div><img className="collections-cards-img" src={Dummy3} alt="" /></div>
+						<div className="collections-cards-text-1">
+							<div>
+								<div>Justin & Stella</div>
+								<div className="collections-cards-text-1-date">28 March 2021</div>
+							</div>
+							<div>
+								<div className="collections-cards-text-1-preview">
+									<AiOutlineEye className="cct1p-img" />
+									<div className="cct1p-text">Preview</div>
+								</div>
+							</div>
+						</div>
+						<div className="collections-cards-text-2">
+							<div className="collections-cards-text-2-imgdow">
+								<BsCardImage className="cct2p-img"/>
+								<div>13</div>
+								<BsBoxArrowInDown className="cct2p-img2"/>
+								<div>3</div>
+							</div>
+							<div className="collections-cards-text-2-edit">		
+								<AiFillEdit className="cct2p-img3"/>
+								<div className="cct2p-text">Edit</div>
+							</div>
+						</div>
+					</div>
+						<div className="collections-cards">
+						<div><img className="collections-cards-img" src={Dummy3} alt="" /></div>
+						<div className="collections-cards-text-1">
+							<div>
+								<div>Justin & Stella</div>
+								<div className="collections-cards-text-1-date">28 March 2021</div>
+							</div>
+							<div>
+								<div className="collections-cards-text-1-preview">
+									<AiOutlineEye className="cct1p-img" />
+									<div className="cct1p-text">Preview</div>
+								</div>
+							</div>
+						</div>
+						<div className="collections-cards-text-2">
+							<div className="collections-cards-text-2-imgdow">
+								<BsCardImage className="cct2p-img"/>
+								<div>13</div>
+								<BsBoxArrowInDown className="cct2p-img2"/>
+								<div>3</div>
+							</div>
+							<div className="collections-cards-text-2-edit">		
+								<AiFillEdit className="cct2p-img3"/>
+								<div className="cct2p-text">Edit</div>
+							</div>
+						</div>
+					</div>
+						<div className="collections-cards">
+						<div><img className="collections-cards-img" src={Dummy3} alt="" /></div>
+						<div className="collections-cards-text-1">
+							<div>
+								<div>Justin & Stella</div>
+								<div className="collections-cards-text-1-date">28 March 2021</div>
+							</div>
+							<div>
+								<div className="collections-cards-text-1-preview">
+									<AiOutlineEye className="cct1p-img" />
+									<div className="cct1p-text">Preview</div>
+								</div>
+							</div>
+						</div>
+						<div className="collections-cards-text-2">
+							<div className="collections-cards-text-2-imgdow">
+								<BsCardImage className="cct2p-img"/>
+								<div>13</div>
+								<BsBoxArrowInDown className="cct2p-img2"/>
+								<div>3</div>
+							</div>
+							<div className="collections-cards-text-2-edit">		
+								<AiFillEdit className="cct2p-img3"/>
+								<div className="cct2p-text">Edit</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 		</>
 	)
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import Dummy2 from '../assets/img/dummy-img/dummy2.png'
 import Header from '../components/Header'
 import Dummy3 from '../assets/img/dummy-img/dummy3.png'
+import Status from '../assets/img/dummy-img/status.png'
+import Baby from '../assets/img/dummy-img/baby.png'
 import { BsCardImage } from 'react-icons/bs'
+import { BsBoxArrowInDown } from 'react-icons/bs'
+import { AiOutlineEye } from 'react-icons/ai'
+import { AiFillEdit } from 'react-icons/ai'
+import { AiOutlineCheck } from 'react-icons/ai'
+import { AiOutlineClose } from 'react-icons/ai'
 
 function Dashboard() {
 	return (
@@ -15,7 +22,321 @@ function Dashboard() {
 				<div className="dashboard-button"><button className="dashboard-button-btn">Edit Profile</button></div>
 			</div>
 		</div>
-		<div className="isi"></div>
+		<div className="dinner-container">
+			<div className="dinner-quick">Quick Access</div>
+			<div className="dinner-rcollections">
+				<div className="dinner-rcollections-title">Recent Collections</div>
+				<div className="dinner-rcollections-cards-container">
+					<div className="dinner-rcollections-cards">
+						<div><img src={Dummy3} alt=""/></div>
+						<div className="dinner-rcollections-cards-info">
+							<div className="dinner-rcollections-cards-info-1">
+								<div>
+									<div className="rcollections-text1">Justin & Stella</div>
+									<div className="rcollections-text2">28 March 2021</div>
+								</div>
+								<div className="rcollections-preview">
+									<AiOutlineEye className="rcollections-preview-img"/>
+									<div className="rcollections-text3">Preview</div>
+								</div>
+							</div>
+							<div className="dinner-rcollections-cards-info-2">
+								<div className="rcollections-imgdow">
+									<BsCardImage className="rcollections-img"/>
+									<div className="rcollections-text4">13</div>
+									<BsBoxArrowInDown className="rcollections-dow"/>
+									<div className="rcollections-text5">3</div>
+								</div>
+								<div className="rcollections-edit">
+									<AiFillEdit className="rcollections-edit-img"/>
+									<div className="rcollections-text6">Edit</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+						<div className="dinner-rcollections-cards">
+						<div><img src={Dummy3} alt=""/></div>
+						<div className="dinner-rcollections-cards-info">
+							<div className="dinner-rcollections-cards-info-1">
+								<div>
+									<div className="rcollections-text1">Justin & Stella</div>
+									<div className="rcollections-text2">28 March 2021</div>
+								</div>
+								<div className="rcollections-preview">
+									<AiOutlineEye className="rcollections-preview-img"/>
+									<div className="rcollections-text3">Preview</div>
+								</div>
+							</div>
+							<div className="dinner-rcollections-cards-info-2">
+								<div className="rcollections-imgdow">
+									<BsCardImage className="rcollections-img"/>
+									<div className="rcollections-text4">13</div>
+									<BsBoxArrowInDown className="rcollections-dow"/>
+									<div className="rcollections-text5">3</div>
+								</div>
+								<div className="rcollections-edit">
+									<AiFillEdit className="rcollections-edit-img"/>
+									<div className="rcollections-text6">Edit</div>
+								</div>
+							</div>
+						</div>
+					</div>
+						<div className="dinner-rcollections-cards">
+						<div><img src={Dummy3} alt=""/></div>
+						<div className="dinner-rcollections-cards-info">
+							<div className="dinner-rcollections-cards-info-1">
+								<div>
+									<div className="rcollections-text1">Justin & Stella</div>
+									<div className="rcollections-text2">28 March 2021</div>
+								</div>
+								<div className="rcollections-preview">
+									<AiOutlineEye className="rcollections-preview-img"/>
+									<div className="rcollections-text3">Preview</div>
+								</div>
+							</div>
+							<div className="dinner-rcollections-cards-info-2">
+								<div className="rcollections-imgdow">
+									<BsCardImage className="rcollections-img"/>
+									<div className="rcollections-text4">13</div>
+									<BsBoxArrowInDown className="rcollections-dow"/>
+									<div className="rcollections-text5">3</div>
+								</div>
+								<div className="rcollections-edit">
+									<AiFillEdit className="rcollections-edit-img"/>
+									<div className="rcollections-text6">Edit</div>
+								</div>
+							</div>
+						</div>
+					</div>
+						<div className="dinner-rcollections-cards">
+						<div><img src={Dummy3} alt=""/></div>
+						<div className="dinner-rcollections-cards-info">
+							<div className="dinner-rcollections-cards-info-1">
+								<div>
+									<div className="rcollections-text1">Justin & Stella</div>
+									<div className="rcollections-text2">28 March 2021</div>
+								</div>
+								<div className="rcollections-preview">
+									<AiOutlineEye className="rcollections-preview-img"/>
+									<div className="rcollections-text3">Preview</div>
+								</div>
+							</div>
+							<div className="dinner-rcollections-cards-info-2">
+								<div className="rcollections-imgdow">
+									<BsCardImage className="rcollections-img"/>
+									<div className="rcollections-text4">13</div>
+									<BsBoxArrowInDown className="rcollections-dow"/>
+									<div className="rcollections-text5">3</div>
+								</div>
+								<div className="rcollections-edit">
+									<AiFillEdit className="rcollections-edit-img"/>
+									<div className="rcollections-text6">Edit</div>
+								</div>
+							</div>
+						</div>
+					</div>
+						<div className="dinner-rcollections-cards">
+						<div><img src={Dummy3} alt=""/></div>
+						<div className="dinner-rcollections-cards-info">
+							<div className="dinner-rcollections-cards-info-1">
+								<div>
+									<div className="rcollections-text1">Justin & Stella</div>
+									<div className="rcollections-text2">28 March 2021</div>
+								</div>
+								<div className="rcollections-preview">
+									<AiOutlineEye className="rcollections-preview-img"/>
+									<div className="rcollections-text3">Preview</div>
+								</div>
+							</div>
+							<div className="dinner-rcollections-cards-info-2">
+								<div className="rcollections-imgdow">
+									<BsCardImage className="rcollections-img"/>
+									<div className="rcollections-text4">13</div>
+									<BsBoxArrowInDown className="rcollections-dow"/>
+									<div className="rcollections-text5">3</div>
+								</div>
+								<div className="rcollections-edit">
+									<AiFillEdit className="rcollections-edit-img"/>
+									<div className="rcollections-text6">Edit</div>
+								</div>
+							</div>
+						</div>
+					</div>
+						<div className="dinner-rcollections-cards">
+						<div><img src={Dummy3} alt=""/></div>
+						<div className="dinner-rcollections-cards-info">
+							<div className="dinner-rcollections-cards-info-1">
+								<div>
+									<div className="rcollections-text1">Justin & Stella</div>
+									<div className="rcollections-text2">28 March 2021</div>
+								</div>
+								<div className="rcollections-preview">
+									<AiOutlineEye className="rcollections-preview-img"/>
+									<div className="rcollections-text3">Preview</div>
+								</div>
+							</div>
+							<div className="dinner-rcollections-cards-info-2">
+								<div className="rcollections-imgdow">
+									<BsCardImage className="rcollections-img"/>
+									<div className="rcollections-text4">13</div>
+									<BsBoxArrowInDown className="rcollections-dow"/>
+									<div className="rcollections-text5">3</div>
+								</div>
+								<div className="rcollections-edit">
+									<AiFillEdit className="rcollections-edit-img"/>
+									<div className="rcollections-text6">Edit</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<div className="dinner-rcollections-seeall">see all collections</div>
+			</div>
+			<div className="dinner-propac-contanier">
+				<div className="dinner-rprojects">
+					<div className="dinner-rprojects-title">Recent Projects</div>
+					<div className="dinner-rprojects-cards-container">
+						<div className="dinner-rprojects-cards">
+							<div className="dinner-rprojects-cards-info">
+								<div className="dinner-rprojects-cards-info1">
+									<div>
+										<div className="rprojects-text1">Justin & Stella</div>
+										<div className="rprojects-text2">28 March 2021</div>
+									</div>
+									<div>
+										<div><img src={Status} alt=""/></div>
+									</div>
+								</div>
+								<div className="dinner-rprojects-cards-info2">
+									<div className="rprojects-checks">
+										<AiOutlineCheck className="rprojects-checks-img"/>
+										<div className="rprojects-checks-text">Packages</div>
+										<AiOutlineCheck className="rprojects-checks-img"/>
+										<div className="rprojects-checks-text">Rundown</div>
+										<AiOutlineCheck className="rprojects-checks-img"/>
+										<div className="rprojects-checks-text">Invoice</div>
+									</div>
+									<div className="rprojects-edit">
+										<AiFillEdit className="rprojects-edit-img"/>
+										<div className="rprojects-edit-text">Edit</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="dinner-rprojects-cards">
+							<div className="dinner-rprojects-cards-info">
+								<div className="dinner-rprojects-cards-info1">
+									<div>
+										<div className="rprojects-text1">Justin & Stella</div>
+										<div className="rprojects-text2">28 March 2021</div>
+									</div>
+									<div>
+										<div><img src={Status} alt=""/></div>
+									</div>
+								</div>
+								<div className="dinner-rprojects-cards-info2">
+									<div className="rprojects-checks">
+										<AiOutlineCheck className="rprojects-checks-img"/>
+										<div className="rprojects-checks-text">Packages</div>
+										<AiOutlineCheck className="rprojects-checks-img"/>
+										<div className="rprojects-checks-text">Rundown</div>
+										<AiOutlineCheck className="rprojects-checks-img"/>
+										<div className="rprojects-checks-text">Invoice</div>
+									</div>
+									<div className="rprojects-edit">
+										<AiFillEdit className="rprojects-edit-img"/>
+										<div className="rprojects-edit-text">Edit</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="dinner-rprojects-cards">
+							<div className="dinner-rprojects-cards-info">
+								<div className="dinner-rprojects-cards-info1">
+									<div>
+										<div className="rprojects-text1">Justin & Stella</div>
+										<div className="rprojects-text2">28 March 2021</div>
+									</div>
+									<div>
+										<div><img src={Status} alt=""/></div>
+									</div>
+								</div>
+								<div className="dinner-rprojects-cards-info2">
+									<div className="rprojects-checks">
+										<AiOutlineCheck className="rprojects-checks-img"/>
+										<div className="rprojects-checks-text">Packages</div>
+										<AiOutlineCheck className="rprojects-checks-img"/>
+										<div className="rprojects-checks-text">Rundown</div>
+										<AiOutlineCheck className="rprojects-checks-img"/>
+										<div className="rprojects-checks-text">Invoice</div>
+									</div>
+									<div className="rprojects-edit">
+										<AiFillEdit className="rprojects-edit-img"/>
+										<div className="rprojects-edit-text">Edit</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<div className="dinner-rprojects-seeall">see all projects</div>
+				</div>
+				<div className="dinner-rpackages">
+					<div className="dinner-rpackages-title">Recent Packages</div>
+					<div className="dinner-rpackages-cards-container">
+						<div className="dinner-rpackages-cards">
+							<div><img src={Baby} alt=""/></div>
+							<div className="dinner-rpackages-cards-info">
+								<div className="dinner-rpackages-cards-info1">
+									<div className="rpackages-text1">Baby Photo Session</div>
+									<div className="rpackages-text2">3 Items</div>
+								</div>
+								<div className="dinner-rpackages-cards-info2">
+									<div className="rpackages-text3">Rp.2,500,000</div>
+									<AiFillEdit className="rpackages-edit"/>
+									<div className="rpackages-text4">Edit</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="dinner-rpackages-cards">
+							<div><img src={Baby} alt=""/></div>
+							<div className="dinner-rpackages-cards-info">
+								<div className="dinner-rpackages-cards-info1">
+									<div className="rpackages-text1">Baby Photo Session</div>
+									<div className="rpackages-text2">3 Items</div>
+								</div>
+								<div className="dinner-rpackages-cards-info2">
+									<div className="rpackages-text3">Rp.2,500,000</div>
+									<AiFillEdit className="rpackages-edit"/>
+									<div className="rpackages-text4">Edit</div>
+								</div>
+							</div>
+						</div>
+						<div className="dinner-rpackages-cards">
+							<div><img src={Baby} alt=""/></div>
+							<div className="dinner-rpackages-cards-info">
+								<div className="dinner-rpackages-cards-info1">
+									<div className="rpackages-text1">Baby Photo Session</div>
+									<div className="rpackages-text2">3 Items</div>
+								</div>
+								<div className="dinner-rpackages-cards-info2">
+									<div className="rpackages-text3">Rp.2,500,000</div>
+									<AiFillEdit className="rpackages-edit"/>
+									<div className="rpackages-text4">Edit</div>
+								</div>
+							</div>
+						</div>
+
+
+					</div>
+					<div className="dinner-rpackages-seeall">see all projects</div>
+				</div>
+			</div>
+		</div>
 		</>
 	)
 }
