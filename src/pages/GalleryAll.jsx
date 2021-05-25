c179518c57e1fbd2be6c51e92b171654866b2d5d
deannovri <novridean@gmail.com>
2021-05-25 22:54:38 +0700

    update

--- src/pages/GalleryAll.jsx
+++ src/pages/GalleryAll.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import HeaderHome from '../components/HeaderHome';
 import Footer from '../components/Footer';
-import Dummy4 from '../assets/img/dummy-img/dummy4.png'
+import Dummy4 from '../assets/img/dummy-img/dummy3.png'
+// import { makeStyles } from '@material-ui/core/styles';
+// import Pagination from '@material-ui/lab/Pagination';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     '& > *': {
+//       marginTop: theme.spacing(2),
+//     },
+//   },
+// }));
 
 function GalleryAll() {
+	// const classes = useStyles();
 	return (
 		<>
 		<HeaderHome/>
 		<div className="gallery-title">Explore Photographer Gallery</div>
 		<div className="gallery-container">
 			<div className="gallery-cards">
-				<div><img src={Dummy4} alt=""/></div>
-				<div>Jamal & Denise Wedding Ceremony</div>
-				<div>Justin Studio</div>
+				<div><img className="gallery-cards-img" src={Dummy4} alt=""/></div>
+				<div className="gallery-cards-text">
+					<div className="gallery-cards-text1">Jamal & Denise Wedding Ceremony</div>
+					<div className="gallery-cards-text2">Justin Studio</div>
+				</div>
 			</div>
+{/*			<div className={classes.root}>
+			      <Pagination count={10} shape="rounded" />
+    		</div>*/}
 		</div>
 		<Footer/>
 		</>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,11 +3,13 @@ import HeaderProps from '../components/HeaderProps'
 import { Form, Button } from 'react-bootstrap';
 import Dummy2 from '../assets/img/dummy-img/dummy2.png'
 import { useDispatch, useSelector } from 'react-redux';
+import { URL_API } from '../helper/url';
+import axios from 'axios';
 
 function Profile() {
-	const [busName, setBusName] = useState();
-	const [addrs, setAddrs] = useState();
-	const [image, setImage] = useState();
+	const [businessName, setBusinessName] = useState();
+	const [address, setAddress] = useState();
+	const [photo, setPhoto] = useState();
 	const [name, setName] = useState();
 	const [email, setEmail] = useState();
 	const auth = useSelector((state) => state.auth);
@@ -18,22 +20,63 @@ function Profile() {
 	}, [])
 
 	const fetchUsers = () => {
-		console.log(auth.name)
+		console.log(auth.photo)
 	}
+
+  function handleEdit(event) {
+    event.preventDefault();
+    var data = {
+      businessName: businessName,
+      address: address,
+      photo: photo,
+      name: name,
+      email: email
+    };
+    var configEdit = {
+      headers: { Authorization: `Bearer ${auth.token}` },
+    };
+    axios
+    .put(`${URL_API}/user`, data, configEdit)
+    .then(() => {
+        dispatch({
+          type: 'EDIT PROFILE',
+          payload: {
+            businessName: businessName,
+            address: address,
+            photo: photo,
+            name: name,
+            email: email
+          },
+        });
+        localStorage.setItem('token', auth);
+    })
+
+  }
 
 	return (
 		<>
 		<HeaderProps title="Edit Profile" link="/dashboard" />
 		<div className="profile-container">
-			<Form className="profile-inner-container">
+			<Form onSubmit={handleEdit} className="profile-inner-container">
 				<div className="profile-bd">Business Details</div>
   				<Form.Group>
     				<Form.Label className="profile-bn">Business Name*</Form.Label>
-    				<Form.Control autoFocus className="custom-form-port" type="text" placeholder="Justin Studio" />
+    				<Form.Control 
+            autoFocus 
+            className="custom-form-port" 
+            type="text"
+            value={auth.businessName}
+            onChange={(e) => setBusinessName(e.target.value)} 
+            placeholder="Justin Studio" />
   				</Form.Group>
     			<Form.Group controlId="exampleForm.ControlTextarea1">
     				<Form.Label className="profile-ad">Address</Form.Label>
-    				<Form.Control className="custom-form-port" as="textarea" rows={4} placeholder="Jl Mekar Sari No 21 Kecamatan Gubeng, Surabaya Jawa Timur 60111"/>
+    				<Form.Control 
+            className="custom-form-port" 
+            as="textarea" rows={4} 
+            placeholder="Jl Mekar Sari No 21 Kecamatan Gubeng, Surabaya Jawa Timur 60111"
+            value={auth.address}
+            onChange={(e) => setAddress(e.target.value)} />
   				</Form.Group>
   				<div>
   					<p className="profile-image">Images</p>
@@ -42,11 +85,21 @@ function Profile() {
   				<div className="profile-ac">Account Details</div>
   				<Form.Group>
     				<Form.Label className="profile-name">Name*</Form.Label>
-    				<Form.Control className="custom-form-port" type="text" placeholder="Justin Junaedi" />
+    				<Form.Control 
+            className="custom-form-port" 
+            type="text" 
+            placeholder="Justin Junaedi" 
+            value={auth.name}
+            onChange={(e) => setName(e.target.value)} />
   				</Form.Group>
    				<Form.Group>
     				<Form.Label className="profile-email">Email*</Form.Label>
-    				<Form.Control className="custom-form-port" type="text" placeholder="studio@justin.com" />
+    				<Form.Control
+            className="custom-form-port" 
+            type="text" 
+            placeholder="studio@justin.com" 
+            value={auth.email}
+            onChange={(e) => setEmail(e.target.value)} />
   				</Form.Group>
   				<div className="profile-button-container">
   					<Button className="profile-button" variant="primary">Save</Button>
